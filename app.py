6e843135757872c2a59767c192a7b79f715c3144
Conner Camp <ccamp123@gmail.com>
2020-11-13 18:47:24 -0700

    created db

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__,static_url_path='/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
