1a80f07c7c192f5043e1fa992633da3b1aa4df05
William Conner Camp <49165849+ccamp123@users.noreply.github.com>
2020-11-13 19:23:34 -0700

    added 'add' functionality to form

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__,static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-# class Ski(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     make = db.Column(db.String(20))
-#     # model = db.Column(db.String(30))
-#     # width = db.Column(db.Integer)
-#     # length = db.Column(db.Integer)
+class Ski(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(20))
+    model = db.Column(db.String(30))
+    width = db.Column(db.Integer)
+    length = db.Column(db.Integer)
 
+db.create_all()
 @app.route('/')
 def homePage():
     return render_template('home.html')
@@ -21,7 +22,15 @@ def createPage():
     return render_template('create.html')
 @app.route('/create',methods=['POST'])
 def create():
-    return 1
+    tempMake = request.form['Make']
+    tempModel = request.form['Model']
+    tempWidth = request.form['Width']
+    tempLength = request.form['Length']
+    tempSki = Ski(make=tempMake,model=tempModel,width=tempWidth,length=tempLength)
+    db.session.add(tempSki)
+    db.session.commit()
+    return render_template('create.html')
+
 @app.route('/read')
 def readPage():
     return render_template('read.html')
